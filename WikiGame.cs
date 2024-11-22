2ca6d441f06e8ee348d4fb40c5f7df36acdb5caf
Livio Theiler <livio.theiler@jmcsoftwareag.onmicrosoft.com>
2024-11-22 10:53:57 +0100

    feat: implement words file functionality

--- WikiGame.cs
+++ WikiGame.cs
@@ -52,26 +52,32 @@ namespace WikiGame
         private void OpenWikipediaPage_CheckTitle(string startPage, string endWord)
         {
             var path = new List<string>();
-            _driver.Navigate().GoToUrl("https://de.wikipedia.org/wiki/Spezial:Zuf%C3%A4llige_Seite");
+            _driver.Navigate().GoToUrl(startPage);
             var bodyContent = string.Empty;
-            while (!bodyContent.Contains("Philosophie", StringComparison.OrdinalIgnoreCase))
+            while (!bodyContent.Contains(endWord, StringComparison.OrdinalIgnoreCase))
             {
                 bodyContent = _driver.FindElement(By.Id("mw-content-text")).GetAttribute("innerHTML");
-                if (bodyContent.Contains("Philosophie", StringComparison.OrdinalIgnoreCase))
+                if (bodyContent.Contains(endWord, StringComparison.OrdinalIgnoreCase))
                 {
-                    Console.WriteLine("The word 'Philosophie' was found in bodyContent.");
+                    Console.WriteLine($"The word '{endWord}' was found in bodyContent.");
                 }
                 else
                 {
+                    IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)_driver;
+                    jsExecutor.ExecuteScript("document.querySelector('#toc')?.remove()");
+                    jsExecutor.ExecuteScript(@"document.querySelectorAll('a img').forEach(img => {
+                                                    const link = img.closest('a');
+                                                    if (link) link.remove(); 
+                                                })");
+
                     var links = _driver.FindElements(By.CssSelector("#mw-content-text a"));
                     IWebElement firstWikiLink = null;
                     
                     foreach (var link in links)
                     {
                         var href = link.GetAttribute("href");
-                        if (href != null 
-                            && href.Contains("/wiki/") 
-                            && !href.Contains("#") 
+                        if (href != null
+                            && href.Contains("/wiki/")
                             && !href.Contains("Datei:")
                             && !href.Contains("Portal:")
                             && !href.Contains("Hilfe:")
@@ -79,13 +85,18 @@ namespace WikiGame
                             && !path.Contains(link.Text))
                         {
                             firstWikiLink = link;
-                            break;
+                            try
+                            {
+                                var text = firstWikiLink.Text;
+                                firstWikiLink.Click();
+                                path.Add(text);
+                                break;
+                            }
+                            catch
+                            {
+                            }
                         }
                     }
-
-                    Assert.IsNotNull(firstWikiLink, "No valid Wikipedia link found in bodyContent.");
-                    path.Add(firstWikiLink.Text);
-                    firstWikiLink.Click();
                 }
             }
 
@@ -93,6 +104,13 @@ namespace WikiGame
             {
                 Console.WriteLine(pathItem);
             }
+
+            string projectDir =
+                Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\.."));
+            var filePath = Path.Combine(projectDir, "Words_result.csv");
+
+            using var writer = new StreamWriter(filePath, true);
+            writer.WriteLine(startPage + "," + endWord + "," + path.Count);
         }
 
         [TearDown]
